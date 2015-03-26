75e4d238e032a3c20b4ba3a75b2b392d642ac6b6
sandyardyan <ardians19m@gmail.com>
2015-03-26 20:48:26 +0700

    Create AlexaRank.js

---
+++ AlexaRank.js
@@ -0,0 +1,2 @@
+<a href="http://www.alexa.com/siteinfo/www.seocips.com"><script src="http://xslt.alexa.com/site_stats/js/s/a?url=http://varietyofindonesian.blogspot.com/" type="text/javascript"></script></a>
+<br />
