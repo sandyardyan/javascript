4be191efb5dcfef91a45318b87ded351d6ce540a
sandyardyan <ardians19m@gmail.com>
2015-03-26 20:52:50 +0700

    Create emoticonblog.js

---
+++ emoticonblog.js
@@ -0,0 +1,1 @@
+<div style="position: fixed; bottom: 0px; right: 20px;width:82px;height:160px;"><a href="" target="_blank"><img src="http://content.sweetim.com/sim/cpie/emoticons/0002013F.gif" border="0" /></a><small><right></right></small></div>
